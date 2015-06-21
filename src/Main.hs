8c397ada87cae5fecdf0e3f9f228d9b45c881e84
lost_mohican <erkinun@gmail.com>
2015-06-21 20:37:16 +0300

    simple gen

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,13 @@
 import System.Environment
 import System.IO
 
+mw = 2
+ios = 3
+android = 3  
+
+type Platform = Int 
+type Days = Int
+
 main = do 
   putStrLn "estimation generator"
   (file:platform:args) <- getArgs
@@ -9,8 +16,18 @@ main = do
   handle <- openFile file ReadMode
   contents <- hGetContents handle
   putStr "estimation for this project: " 
-  print (estimate contents)
-  
+  print (estimate contents $ findPL platform)
 
-estimate :: String -> Int
-estimate contents = 10 
+--roadmap 
+--0 use platform constants
+--1 get each sentence in a line 
+--2 get each verb in a sentence 
+--3 find special words like payment, integration, transfer, chat, video, help etc
+
+estimate :: String -> Platform -> Days
+estimate contents pl  = pl * (length $ lines contents)
+
+findPL :: String -> Platform 
+findPL "mw" = mw 
+findPL "ios" = ios
+findPL "android" = android
