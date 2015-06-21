d874e199e01091983d83bca61204924d4dce754d
lost_mohican <erkinun@gmail.com>
2015-06-21 20:18:12 +0300

    read file and platform name

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,16 @@
-main = putStrLn "estimation generator"
+import System.Environment
+import System.IO
+
+main = do 
+  putStrLn "estimation generator"
+  (file:platform:args) <- getArgs
+  putStrLn "starting to estimate with file"
+  putStrLn file
+  handle <- openFile file ReadMode
+  contents <- hGetContents handle
+  putStr "estimation for this project: " 
+  print (estimate contents)
+  
+
+estimate :: String -> Int
+estimate contents = 10 
